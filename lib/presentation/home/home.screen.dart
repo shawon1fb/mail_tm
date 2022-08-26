96dd28dd9fca5741a3654d0ce04e3926491f0b7a
shawon1fb <shawon0187@gmail.com>
2022-08-26 14:11:04 +0600

    message card

--- lib/presentation/home/home.screen.dart
+++ lib/presentation/home/home.screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
+import '../../domain/core/models/messages.data.model.dart';
 import '../../values/values.dart';
 import '../shared/scroll/refress_scroll.dart';
 import '../shared/widgets/custom_shape_clippers.dart';
 import 'controllers/home.controller.dart';
 
 class HomeScreen extends GetView<HomeController> {
-   HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
 
   // bool onCheck = false;
 
@@ -37,15 +39,90 @@ class HomeScreen extends GetView<HomeController> {
             return RefreshScroll(
               scrollController: scrollController,
               child: ListView.builder(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
                 controller: scrollController,
                 itemCount: logic.messages.length,
                 itemBuilder: (context, i) {
-                  return Text('$i');
+                  return MessagesCard(item: logic.messages[i]);
                 },
               ),
             );
           },
         ),
+      ),
+    );
+  }
+}
+
+class MessagesCard extends StatelessWidget {
+  const MessagesCard({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
+
+  final MessageDataModel item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 1.sw,
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      decoration: BoxDecoration(
+        color: const Color(0xff607EAA),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            'ID: ${item.id}',
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          5.verticalSpace,
+          Text(
+            'From'.toUpperCase(),
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          5.verticalSpace,
+          Text('email: ${item.from.address}',
+              style: const TextStyle(color: Color(0xffF9F5EB))),
+          Text('name: ${item.from.name}',
+              style: const TextStyle(color: Color(0xffF9F5EB))),
+          10.verticalSpace,
+          Text(
+            'TO'.toUpperCase(),
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          5.verticalSpace,
+          for (int i = 0; i < item.to.length; i++)
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text('email: ${item.to[i].address}',
+                    style: const TextStyle(color: Color(0xffF9F5EB))),
+                Text('name: ${item.to[i].name}',
+                    style: const TextStyle(color: Color(0xffF9F5EB))),
+                5.verticalSpace,
+              ],
+            ),
+          Text('subject: ${item.subject}',
+              style: const TextStyle(color: Color(0xffF9F5EB))),
+          Text('intro: ${item.intro}',
+              style: const TextStyle(color: Color(0xffF9F5EB))),
+        ],
       ),
     );
   }
